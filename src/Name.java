1e195d697040600fd48f67467856dfab4fc4b7c8
mnakamoto0305 <m.nakamoto0305@gmail.com>
2021-03-21 14:19:16 +0900

    firstcommit

---
+++ src/Name.java
@@ -0,0 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Name {
+
+	public static String toSnakeCase(String name) {
+		if (name == null) {
+			throw new NullPointerException("text = null.");
+		}
+		String snake = name;
+		Pattern p = Pattern.compile("([A-Z])");
+		while (true) {
+			Matcher m = p.matcher(snake);
+			if (!m.find()) break;
+			snake = m.replaceFirst("_" + m.group(1).toLowerCase());
+		}
+		return snake.replaceFirst("^_", "");
+	}
+}
---
+++ src/NameTest.java
@@ -0,0 +1,35 @@
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Test;
+
+public class NameTest extends Name {
+
+	@Test
+	public void aaaはaaaのまま出力される() throws Exception {
+		// Exercise
+		String expected = "aaa";
+		String actual = Name.toSnakeCase("aaa");
+
+		// Verify
+		assertThat(actual, is(expected));
+	}
+
+	@Test
+	public void HelloWorldはhello_worldに変換される() throws Exception {
+		// Exercise
+		String expected = "hello_world";
+		String actual = Name.toSnakeCase("HelloWorld");
+		// Verify
+		assertThat(actual, is(expected));
+	}
+
+	@Test
+	public void practiceJUnitはpractice_junitに変換される() throws Exception {
+		// Exercise
+		String expected = "practice_junit";
+		String actual = Name.toSnakeCase("practiceJunit");
+		// Verify
+		assertThat(actual, is(expected));
+	}
+}
